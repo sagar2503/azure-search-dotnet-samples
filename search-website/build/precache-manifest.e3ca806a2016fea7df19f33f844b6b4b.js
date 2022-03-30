686a49cc272395dd8c497933a97f7dc44dcc4b13
sagar2503 <sagar2503@users.noreply.github.com>
2022-03-30 12:19:41 +0530

    changes

---
+++ search-website/build/precache-manifest.e3ca806a2016fea7df19f33f844b6b4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24918d08c34b91772d2e4910eed001c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15115978ecb47d931aad",
+    "url": "/static/css/2.9c82a2bb.chunk.css"
+  },
+  {
+    "revision": "0794dae7e0b104f3a717",
+    "url": "/static/css/main.43ab7a72.chunk.css"
+  },
+  {
+    "revision": "15115978ecb47d931aad",
+    "url": "/static/js/2.8089b5f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8089b5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0794dae7e0b104f3a717",
+    "url": "/static/js/main.b2a3f736.chunk.js"
+  },
+  {
+    "revision": "0c88c9ae059e59d0ebbf",
+    "url": "/static/js/runtime-main.37a6d1fe.js"
+  }
+]);
